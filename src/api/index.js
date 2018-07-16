5b4a2fd41de15f9ab7a3961d22b09ccf266ffadc
rilhad1 <rilhad1@gmail.com>
2018-07-17 01:47:48 +0300

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const { update, create, remove, list, findOne } = require('../controllers/company/');
+const { update, create, remove, list } = require('../controllers/company/');
 
 const router = express.Router();
 
 router.get('/', list);
-router.get('/find', findOne);
 router.post('/', create);
 router.put('/', update);
 router.delete('/', remove);
